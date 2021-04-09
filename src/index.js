fd80b13e9452e6dd20e2ec8291cf7d29adda9edf
lenakernytska <yaapril222@gmail.com>
2021-04-10 00:19:48 +0300

    Adds contacts-operations

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./App";
+import 'modern-normalize/modern-normalize.css';
+import './style.css';
+import { Provider } from 'react-redux';
+import {store} from "./redux/store";
+// import { PersistGate } from 'redux-persist/integration/react';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      {/* <PersistGate loading={<h1>Loading...</h1>} persistor={persistor}> */}
+        <App />
+        {/* </PersistGate> */}
+      </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
